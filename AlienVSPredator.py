cb3e5be77129effd1b766b5d4d0514bd0756ff29
Christian Lombardi <96151080+Znorlux@users.noreply.github.com>
2023-04-20 22:00:31 -0500

    Add files via upload

--- AlienVSPredator.py
+++ AlienVSPredator.py
@@ -1,4 +1,5 @@
 import random
+import time
 class Node:
   def __init__(self, value):
     self.value = value
@@ -83,8 +84,9 @@ class Tablero:
 
     for i in range(self.n):
       fila = DLinkedList() #Creamos filas que seran listas doblemente enlazadas
-      for j in range(self.n):
+      for j in range(self.n): 
         #print((i,j))
+        
         if (i,j) in depredador_coord:
           fila.add_at_head(depredador) #add_at_head sirve para añadir un elemento a la ultima posicion de la lista enlazada
         elif (i,j) in alien_coord:
@@ -127,18 +129,37 @@ class Tablero:
         current = current.prev
 
 class Juego:
-  def __init__(self, tablero) -> None:
+  def __init__(self, tablero, controlador) -> None:
     self.tablero = tablero
-    self.realizar_movimiento()
-     
-  def realizar_movimiento(self):
+    tablero.imprimir_tablero()
+    self.controlador_juego(controlador)
+  
+  def controlador_juego(self,controlador):
+    if controlador == False:
+      self.obtener_movimiento_alien()
+    elif controlador == True:
+      print()
+      print("El juego ha finalizado")
+      exit()
+
+  def obtener_movimiento_alien(self):
     #Averiguamos en que coordenada está el alien para decirle a donde se puede mover 
-    coordenadas = alien.coordenadas[0]#Tupla con las coordenadas del alien
+    coordenadas = alien.coordenadas[0]#Lista con Tupla con las coordenadas del alien
     #Sacamos los movimientos del alien
     movimiento_izquierda = (coordenadas[0], coordenadas[1]-1)
     movimiento_derecha = (coordenadas[0], coordenadas[1]+1)
     movimiento_arriba = (coordenadas[0]-1, coordenadas[1])
     movimiento_abajo = (coordenadas[0]+1, coordenadas[1])
+    posibles_ataques= [movimiento_arriba, movimiento_abajo, movimiento_derecha, movimiento_izquierda]
+    if depredador.coordenadas[0] in posibles_ataques:
+      while True:
+        atacar = input("El depredador se encuentra en su rango de ataque, desea atacarlo? (s/n) ").lower()
+        if atacar == "s":
+          depredador.vida -= 10
+          print("La vida del depredador disminuyó a ", depredador.vida)
+          break
+        elif atacar == "n":
+          break
     movimientos_disponibles = ["arriba", "abajo", "derecha", "izquierda"]
     #Si está en la fila 0 entonces no podrá ir para arriba
     if coordenadas[0] == 0:
@@ -188,27 +209,158 @@ class Juego:
         direccion = movimiento_izquierda
         break
     print("Tu direccion de movimiento es ",nuevo_mov, ", con coordenadas ",direccion)
-    self.actualizar_matriz(direccion,alien)
+    self.mover_alien(direccion,alien)
 
-  def actualizar_matriz(self, direccion,alien):
-     current = tablero.matriz.trailer.prev
-     fila = current.value
-     node = fila.trailer.prev 
-     for i in range(tablero.n):
+  def mover_alien(self, direccion,alien):
+    direccion = [direccion]
+    current = tablero.matriz.trailer.prev
+    fila = current.value
+    node = fila.trailer.prev
+    for i in range(tablero.n):
       for j in range(tablero.n):
-        if (i,j) == direccion:
-          print(node.value)
-          node.value = alien.simbolo
-          #node.value = alien.simbolo
-          #alien.coordenadas = ' . '
-    
+        #print("coordenadas: ",(i,j))
+        #print("NODO:", node.value)
+        #Verificamos que si el nodo es None, lo que significa que ya se llego al final de la fila y se debe pasar a la siguiente
+        if node.value == None:
+          current = current.prev
+          fila = current.value
+          node = fila.trailer.prev
+          
+        #Verificamos haber llegado a la coordenada correcta, y de esta manera colocamos al Alien ahí
+        if (i,j) in direccion:
+          #Verificamos que hay en el value del nodo, para saber si se le resta vida al alien o no
+          if node.value == " + ":
+            alien.vida += 10
+            print("Tu vida ha aumentado 10 puntos, ahora tienes: ",alien.vida, "de vida")
+          elif node.value == ' - ':
+            alien.vida -= 10
+            #if alien.vida >
+            print("Tu vida ha disminuido 10 puntos, ahora tienes: ",alien.vida, "de vida")
+          elif isinstance(node.value, Depredador):
+            print("Por qué intentas suicidarte?")
+            alien.vida -= 25
+            print("Tu vida ha disminuido a ", alien.vida,"por intentar estar en el mismo lugar que el depredador!")
+            tablero.imprimir_tablero()
+            self.obtener_movimiento_depredador()
+          node.value =  alien
+        node = node.prev
+    if(self.end_game()):
+      self.estado_de_juego(True)
+    self.borrar_alien(alien, direccion)
 
+  def borrar_alien(self,alien, nuevas_coord):
+    current = tablero.matriz.trailer.prev
+    fila = current.value
+    node = fila.trailer.prev
+    for i in range(tablero.n):
+      for j in range(tablero.n):
+        if node.value == None:
+          current = current.prev
+          fila = current.value
+          node = fila.trailer.prev
+        if (i,j) in alien.coordenadas:
+          node.value = ' . '
+        node = node.prev
+    #Actualizamos las coordenadas del alien
+    alien.coordenadas = nuevas_coord
+    tablero.imprimir_tablero()
+    self.obtener_movimiento_depredador()
+
+  def obtener_movimiento_depredador(self):
+    coordenadas = depredador.coordenadas[0]#Lista con Tupla con las coordenadas del depredador
+    #Sacamos los movimientos del depreadador
+    movimiento_izquierda = (coordenadas[0], coordenadas[1]-1)
+    movimiento_derecha = (coordenadas[0], coordenadas[1]+1)
+    movimiento_arriba = (coordenadas[0]-1, coordenadas[1])
+    movimiento_abajo = (coordenadas[0]+1, coordenadas[1])
+    diagonal_arriba_derecha = (movimiento_arriba[0], movimiento_arriba[1]+1)
+    diagonal_arriba_izquierda = (movimiento_arriba[0], movimiento_arriba[1]-1)
+    diagonal_abajo_derecha = (movimiento_abajo[0], movimiento_abajo[1]+1)
+    diagonal_abajo_izquierda = (movimiento_abajo[0], movimiento_abajo[1]-1)
+
+    movimientos_disponibles = [movimiento_abajo, movimiento_arriba, movimiento_derecha, movimiento_izquierda, diagonal_arriba_derecha, diagonal_arriba_izquierda, diagonal_abajo_derecha, diagonal_abajo_izquierda]
+    #print(movimientos_disponibles)
+    movimientos_validos = []
+    for movimiento in movimientos_disponibles:
+        if not (-1 in movimiento or tablero.n in movimiento):
+            movimientos_validos.append(movimiento)
+    movimiento = random.choice(movimientos_validos)
+    self.mover_depredador(movimiento)
+  
+  def mover_depredador(self, coordenadas):
+    print()
+    print("Es turno del depredador, preparando su movimiento...")
+    time.sleep(3)
+    print("El depredador se mueve a las coordenadas ", coordenadas)
+    coordenadas = [coordenadas]
+    current = tablero.matriz.trailer.prev
+    fila = current.value
+    node = fila.trailer.prev
+    for i in range(tablero.n):
+      for j in range(tablero.n):
+        if node.value == None:
+          current = current.prev
+          fila = current.value
+          node = fila.trailer.prev
+        if (i,j) in coordenadas:
+          if node.value == " + ":
+            depredador.vida += 10
+            print("La vida del depredador ha aumentado 10 puntos, ahora tiene: ",depredador.vida, "de vida")
+          elif node.value == ' - ':
+            depredador.vida -= 10
+            print("La vida del depredador ha disminuido 10 puntos, ahora tiene: ",depredador.vida, "de vida")
+          elif isinstance(node.value, Alien):
+            alien.vida -= 25
+            print("El depredador ha atacado al alien, la vida del alien ahora es ", alien.vida)
+            tablero.imprimir_tablero()
+            print()
+            self.estado_de_juego(self.end_game())
+          node.value = depredador
+        node = node.prev
+    if(self.end_game()):
+      self.estado_de_juego(True)
+    self.borrar_depredador(depredador, coordenadas)
+
+  def borrar_depredador(self, depredador, nuevas_coord):
+    current = tablero.matriz.trailer.prev
+    fila = current.value
+    node = fila.trailer.prev
+    for i in range(tablero.n):
+      for j in range(tablero.n):
+        if node.value == None:
+          current = current.prev
+          fila = current.value
+          node = fila.trailer.prev
+        if (i,j) in depredador.coordenadas:
+          node.value = ' . '
+        node = node.prev
+    #Actualizamos las coordenadas del alien
+    depredador.coordenadas = nuevas_coord
+    tablero.imprimir_tablero()
+    print()
+    self.estado_de_juego(self.end_game())
+
+  def end_game(self):
+    if depredador.vida <= 0:
+      print("El alien ha ganado!")
+      return True
+    elif alien.vida <= 0:
+      print("El Depreador ha ganado!")
+      return True
+    else:
+      return False
     
+  def estado_de_juego(self, estado):
+    if estado == True:
+      self.controlador_juego(True)
+    else:
+      self.controlador_juego(False)
+  
 alien = Alien()
 depredador = Depredador()
 tablero = Tablero(6)
 tablero.crear_tablero(alien, depredador)
-tablero.imprimir_tablero()
+#tablero.imprimir_tablero()
 
-Controlador = Juego(tablero)
+Controlador = Juego(tablero, False)
 tablero.imprimir_tablero()
